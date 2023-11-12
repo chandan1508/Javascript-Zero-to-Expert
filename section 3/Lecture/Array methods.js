86f330d96853dff4c4a8d0c9f5e73db831b0da5a
chandan <chandankuma8001360@gmail.com>
2023-11-12 12:51:43 +0530

    basic array methods

---
+++ section 3/Lecture/Array methods.js
@@ -0,0 +1,13 @@
+const fruit = [];
+fruit.push("mango", "apple", "banana");
+console.log(fruit);
+fruit.push("papaya");
+console.log(fruit);
+fruit.pop();
+console.log(fruit);
+fruit.unshift("orange");
+console.log(fruit);
+fruit.unshift("Grapes");
+console.log(fruit);
+fruit.shift();
+console.log(fruit);
