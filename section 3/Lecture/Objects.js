f2189a0f32801e88b857ffd082302ab618d481f6
chandan <chandankuma8001360@gmail.com>
2023-11-13 11:24:03 +0530

    more in objects

--- section 3/Lecture/Objects.js
+++ section 3/Lecture/Objects.js
@@ -1,15 +1,29 @@
 const chandan = {
   firstName: "Chandan",
   lastName: "Mukhiya",
-  age: 2037 - 1991,
+  birthyear: 1991,
   job: "Teacher",
   friends: ["Jitender", "rahul", "ravi"],
+  hasDriversLicense: true,
+
+  calcAge: function () {
+    this.age = 2037 - this.birthyear;
+    return this.age;
+  },
+
+  getSummery: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      this.hasDriversLicense ? "a" : "no"
+    } driver's license.`;
+  },
 };
-console.log(chandan);
+// console.log(chandan);
 
-console.log(chandan.lastName);
-console.log(chandan["lastName"]);
+// console.log(chandan.lastName);
+// console.log(chandan["lastName"]);
 
-const nameKey = "Name";
-console.log(chandan["first" + nameKey]);
-console.log(chandan["last" + nameKey]);
+// const nameKey = "Name";
+// console.log(chandan["first" + nameKey]);
+// console.log(chandan["last" + nameKey]);
+
+console.log(chandan.getSummery());
