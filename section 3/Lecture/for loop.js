7777abe235c2e7ceef025b0018d4afc2d6e03db4
chandan <chandankuma8001360@gmail.com>
2023-11-15 19:33:39 +0530

    for loop in javascript

---
+++ section 3/Lecture/for loop.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i}`);
+}
+
+const chandan = ["chandan", "mukhiya", 23, ["ravi", "jatin", "kundan"], true];
+
+for (let i = 0; i < chandan.length; i++) {
+  console.log(chandan[i], typeof chandan[i]);
+}
